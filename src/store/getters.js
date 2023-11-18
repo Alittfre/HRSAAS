9a5adb183633878a18da8b17e1d3c356fd4ad7b9
Alittfre <alittfre@gmail.com>
2023-11-18 21:37:30 +0800

    complete login interface

--- src/store/getters.js
+++ src/store/getters.js
@@ -1,5 +1,6 @@
 const getters = {
   sidebar: state => state.app.sidebar,
-  deivce: state => state.app.deivce
+  deivce: state => state.app.deivce,
+  token: state => state.user.token
 }
 export default getters
