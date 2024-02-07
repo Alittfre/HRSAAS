5970810cba3bcffdc6ed22f92bfaf0f39d4a8f5a
Alittfre <alittfre@gmail.com>
2024-02-07 17:49:33 +0800

    update

--- src/views/employees/index.vue
+++ src/views/employees/index.vue
@@ -4,7 +4,7 @@
       <page-tools :show-before="true">
         <span slot="before">共{{ page.total }}条数据</span>
         <template slot="after">
-          <el-button size="small" type="warning">excel导入</el-button>
+          <el-button size="small" type="warning" @click="$router.push('/import?type=user')">excel导入</el-button>
           <el-button size="small" type="danger">excel导出</el-button>
           <el-button
             size="small"
@@ -37,7 +37,7 @@
           </el-table-column>
           <el-table-column label="操作" sortable="" fixed="right" width="280">
             <template slot-scope="{ row }">
-              <el-button type="text" size="small">查看</el-button>
+              <el-button type="text" size="small" @click="$router.push(`/employees/detail/${obj.row.id}`)">查看</el-button>
               <el-button type="text" size="small">转正</el-button>
               <el-button type="text" size="small">调岗</el-button>
               <el-button type="text" size="small">离职</el-button>
@@ -76,6 +76,7 @@
 import { getEmployeeList, delEmployee } from '@/api/employees'
 import EmployeeEnum from '@/api/constant/employees'
 import addEmployee from './components/add-employee.vue'
+import { formatDate } from '@/filters'
 export default {
   components: { addEmployee },
   data() {
@@ -121,6 +122,42 @@ export default {
       } catch (e) {
         this.$message.error(e)
       }
+    },
+    exportData() {
+      const headers = {
+        '姓名': 'username',
+        '手机号': 'mobile',
+        '入职日期': 'timeOfEntry',
+        '聘用形式': 'formOfEmployment',
+        '转正日期': 'correctionTime',
+        '工号': 'workNumber',
+        '部门': 'departmentName'
+      }
+
+      import('@/vendor/Export2Excel').then(async excel => {
+        const { rows } = await getEmployeeList({ page: 1, size: this.page.total })
+        const data = this.formatJson(headers, rows)
+        excel.export_json_to_excel({
+          header: Object.keys(headers),
+          data,
+          filename: '员工信息表',
+          autoWidth: true,
+          bookType: 'xlsx'
+        })
+      })
+    },
+    formatJson(headers, rows) {
+      return rows.map(item => {
+        return Object.keys(headers).map(key => {
+          if (headers[key] === 'timeOfEntry' || headers[key] === 'correctionTime') {
+            return formatDate(item[headers[key]]) // 返回格式化之前的时间
+          } else if (headers[key] === 'formOfEmployment') {
+            var en = EmployeeEnum.hireType.find(obj => obj.id === item[headers[key]])
+            return en ? en.value : '未知'
+          }
+          return item[headers[key]]
+        })
+      })
     }
   }
 }
